57197b0c437497a4759c77f2eb682a3232240e8b
AkhilBalireddy <akhilreddy2109@gmail.com>
2018-11-06 21:59:59 +0530

    CProgramforSumofTwoNumbers

---
+++ Programming/C/SumofTwoNumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int a,b,c;
+clrscr;
+printf("enter value for a");
+scanf("%d",&a);
+printf("enter value for b");
+scanf("%d",&b);
+c=a+b;
+printf("sum=%d",&c);
+getch();
+return 0;
+}
