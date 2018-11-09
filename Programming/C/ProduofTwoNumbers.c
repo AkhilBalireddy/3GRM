1b14604515660fd909dbfa0b8636ae9d03a9a2df
AkhilBalireddy <akhilreddy2109@gmail.com>
2018-11-09 19:34:09 +0530

    C_Program_for_Product_of_Two_Numbers

---
+++ Programming/C/ProduofTwoNumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter value for a");
+    scanf("%d",&a);
+    printf("enter value for b");
+    scanf("%d",&b);
+    c=a*b;
+    printf("c=%.2d",c);
+    return 0;
+}
