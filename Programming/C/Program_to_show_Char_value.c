a1005b3377e34bf0db396d6c8ebfcb9fd090e6a8
AkhilBalireddy <akhilreddy2109@gmail.com>
2019-01-05 21:53:01 +0530

    Program_to_show_Char_value

---
+++ Programming/C/Program_to_show_Char_value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter char\n");
+scanf("%c",&ch);
+
+printf("Hello\n");
+if(ch>=97 && ch<=122)
+
+//if(ch>='a' && ch<='z')
+printf("ch=%c and its ASCII=%d\n",ch,ch);
+printf("Hai\n");
+}
+
+
+
