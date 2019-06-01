e4f7804977a831c2a5f0a2585aa55ce324746c08
AkhilBalireddy <akhilreddy2109@gmail.com>
2019-06-02 00:41:17 +0530

    updated program

--- Programming/C/Function_to_Print_binaryFormat.c
+++ Programming/C/Function_to_Print_binaryFormat.c
@@ -3,15 +3,15 @@ void print_binary(int);		//Function Desclarartion
 int main()
 {
 	int num;
-	printf("enter the number");	//to Take the Number
-	scanf("%d",&num);		//To scan the number which we given
+	printf("Enter the Number:-");	//to Take the Number
+	scanf("%d", &num);		//To scan the number which we given
 	print_binary(num);		//function call
 	printf("\n");
 }
 void print_binary(int n)		//function declaration
 {
 	int pos;
-	for(pos=31;pos>=0;pos--)		//Loop rotation for 32 bits
-		printf("%d",n>>pos&1); 		//to print numbers in binary
+	for(pos=31; pos>=0; pos--)		//Loop rotation for 32 bits
+		printf("%d", n>>pos&1); 		//to print numbers in binary
 }
 
