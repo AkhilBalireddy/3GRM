45f60281ad9caf0cd3b97a6f22226ec6ba3a25cd
AkhilBalireddy <akhilreddy2109@gmail.com>
2019-06-02 00:12:49 +0530

    C_Program_to_know_use_of_Relational_Operations.c

--- Programming/C/C_Program_to_know_use_of_Relational_Operations.c
+++ Programming/C/C_Program_to_know_use_of_Relational_Operations.c
@@ -2,24 +2,24 @@
 int main()
 {
 	int number1,number2;
-	printf("Enter Values for a and b");
-	scanf("%d%d", &number1 &number2);       //to scan the variables
-	if(a<b)            		 //Condition to check lessthan
+	printf("Enter Values for number1 and number2:-");
+	scanf("%d %d", &number1,&number2);       //to scan the variables
+	if(number1 < number2)            		 //Condition to check lessthan
 		printf("%d is lessthan %d\n", number1,number2);
 
-	if(a<=b)           		 //condition to check lessthan or equal to
-		printf("%d is lessthan or equal to %d\n" number1,number2);
+	else if(number1 <= number2)           		 //condition to check lessthan or equal to
+		printf("%d is lessthan or equal to %d\n", number1,number2);
 
-	if(a==b)           		 //condition to check equal to
+	else if(number1 == number2)           		 //condition to check equal to
 		printf("%d is equal to %d\n", number1,number2);
 
-	if(a!=b)           		 //Condition to check not equal to
+	else if(number1 != number2)           		 //Condition to check not equal to
 		printf("%d is not equal to %d\n", number1,number2);
 
-	if(a>b)             		//Condition to check greater then
+	else if(number1 > number2)             		//Condition to check greater then
 		printf("%d is greater than %d\n", number1,number2);
 
-	if(a>=b)           		 //Condition to check greater than or equal to
+	else if(number1 >= number2)           		 //Condition to check greater than or equal to
 		printf("%d is greater than or equal to %d\n", number1,number2);
 	return 0;
 }
