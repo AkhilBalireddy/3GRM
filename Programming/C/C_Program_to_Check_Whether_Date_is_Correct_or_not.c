cfb58abbd24ee8df82bc52d442b00932e0cad6d6
AkhilBalireddy <akhilreddy2109@gmail.com>
2019-06-01 23:58:03 +0530

    C_Program_to_Check_Whether_Date_is_Correct_or_not.c

--- Programming/C/C_Program_to_Check_Whether_Date_is_Correct_or_not.c
+++ Programming/C/C_Program_to_Check_Whether_Date_is_Correct_or_not.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int date, month, year;
+	int date, month, year,y;
 	int flag=1, isleap=0;
 	printf("Enter date in the form of (dd/mm/yyyy):- ");
 	scanf("%d %d %d", &date,&month,&year);
-	if(y%100!=0 &&y%4==0 || y%400==0)   //to check the leap year
+	if(y%100!=0 &&(y%4==0 || y%400==0))   //to check the leap year
 		isleap=1;
-	if(year<1850 || year>2050 || month<1 || m>onth12 ||date<1 ||date>31)
+	if(year<1850 || year>2050 || month<1 || month>12 ||date<1 ||date>31)
 		flag=0;
 	else if(month==2)       //Check for number of days in feb
 	{
